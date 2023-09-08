48d5f76484bf577cc05d81d64a832e0d470e11d0
Sujith <ps335@snu.edu.in>
2023-09-08 15:44:17 +0530

    shift cipher done

--- lab1/shift_cipher.py
+++ lab1/shift_cipher.py
@@ -1,23 +1,26 @@
-import index_of_coincidence
+import index_of_coincidence, re
 
 def break_shift(cipher):
+
+    cipher = cipher.upper()
+    cipher = re.sub(r'[^A-Z]', '', cipher)
 
     # Calculate the index of coincidence for each shift
     shifts = {}
     for shift in range(26):
-        shifted_text = ''.join([chr((ord(c) - 65 - shift) % 26 + 65) for c in cipher])
+        shifted_text = ''.join(
+            [chr((ord(c) - 61 - shift) % 26 + 61) for c in cipher]
+        )
         shifts[shift] = index_of_coincidence.index_of_coincidence(shifted_text)
 
-
     min_diff = min(abs(shifts[shift] - 0.065) for shift in shifts)
-    print(shifts)
+    
     for shift in shifts:
         if abs(shifts[shift] - 0.065) == min_diff:
             return shift
     
 
 
-# cipher ='svyltpwzbtkvsvyzpahtlajvuzljalabyhkpwpzjpunlspatvyiplbsvyltzlkubujchypbzclopjbshpklaulxblwohzlssbzqbzavupiowvzblylcpahlpwzbtlabsshtjvywlythslzbhkhlspambzjlwlsslualzxblbyuhwshjlyhahualcbswbahalzlkjbyzbztpwohylayhjshzzhwaluaahjpapzvjpvzxbhkspavyhavyxbluawlyjvubiphuvzayhwlypujlwavzoptluhlvztvyipzlkalssbzzlklyvzzltwlykpnupzzpttvyipbsshtjvywlyolukylypajvukptluabtkvuljmypunpsshslvlaubujwvyaapavycpahlwlsslualzxblsljabzjvuzlxbhakvuljwohylayhwohylayhbsshtjvywlyuhtypzbzsvyltclzapibsbtlabsshtjvywlyuljjvunbluljlzabaaltwvyluptupzphajvuzljalabyavyavyyovujbzlnlazlkmhbjpibzishukpahbnblpkmlbnphaubujzbzjpwpacpahlcpchtbzbabyuhlbypzbzaltwbzthaapzwyvpubaupzpxbpzhbnblzvsspjpabkpulsltluabtbahjbyuhlaphtapujpkbuaupzppwzbtpumlytluabtalssbzwshjlyhamhbjpibzthbypzwbscpuhywvyahhualsvivyapzaypzapxblthnuhlsltluabtpkzlkzlkipilukbtlupt'
 cipher = input('Enter the cipher text: ')
 shift = break_shift(cipher)
 print('The shift is', shift)
