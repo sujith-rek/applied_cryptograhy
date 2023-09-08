c2efbb08ad34620c3a19298a072fc2c965d371cc
Sujith <ps335@snu.edu.in>
2023-09-08 18:34:49 +0530

    complete kaisiskis test

--- lab1/kaisiski_test.py
+++ lab1/kaisiski_test.py
@@ -1,32 +1,29 @@
 import math
 
 def kaisiski_test(cipher):
-    # Find the first 3 repeated trigrams
-    trigrams = {}
-    for i in range(len(cipher) - 3):
-        trigram = cipher[i:i+3]
-        if trigram in trigrams:
-            trigrams[trigram].append(i)
+    
+    bigrams = {}
+    for i in range(len(cipher) - 1):
+        if cipher[i:i+2] not in bigrams:
+            bigrams[cipher[i:i+2]] = [i]
         else:
-            trigrams[trigram] = [i]
-    repeated_trigrams = {}
-    for trigram in trigrams:
-        if len(trigrams[trigram]) > 1:
-            repeated_trigrams[trigram] = trigrams[trigram]
-    # Find the distances between the repeated trigrams
+            bigrams[cipher[i:i+2]].append(i)
+    
     distances = {}
-    for trigram in repeated_trigrams:
-        for i in range(len(repeated_trigrams[trigram]) - 1):
-            distance = repeated_trigrams[trigram][i+1] - repeated_trigrams[trigram][i]
-            if distance in distances:
-                distances[distance] += 1
-            else:
-                distances[distance] = 1
-    # Find the greatest common divisor of the distances
+    for bigram in bigrams:
+        if len(bigrams[bigram]) > 1:
+            distances[bigram] = []
+            for i in range(len(bigrams[bigram]) - 1):
+                distances[bigram].append(bigrams[bigram][i+1] - bigrams[bigram][i])
+            
     gcd = 0
-    for distance in distances:
-        if gcd == 0:
-            gcd = distance
-        else:
-            gcd = math.gcd(gcd, distance)
+
+    for bigram in distances:
+        for i in range(len(distances[bigram])):
+            for j in range(i+1, len(distances[bigram])):
+                gcd = math.gcd(distances[bigram][i], distances[bigram][j])
+                if gcd > 1:
+                    return gcd
+    
     return gcd
+
