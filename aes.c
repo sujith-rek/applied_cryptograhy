0658b35a670b9e1523bb69da7efa4c752a45369f
Sujith <ps335@snu.edu.in>
2024-03-27 12:12:09 +0530

    fix encrypt and decrypt along with s-box

--- aes.c
+++ aes.c
@@ -1,14 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define BLOCK_SIZE 16
 #define FIRST_ROW 0
 #define SECOND_ROW 1
 #define THIRD_ROW 2
 #define FOURTH_ROW 3
+#define N_ROUNDS 10
+#define RCONST_1 0x01
+#define RCONST_2 0x02
+#define RCONST_3 0x04
+#define RCONST_4 0x08
+#define RCONST_5 0x10
+#define RCONST_6 0x20
+#define RCONST_7 0x40
+#define RCONST_8 0x80
+#define RCONST_9 0x1B
+#define RCONST_10 0x36
+#define MUL2(x) ((x << 1) ^ (((x >> 7) & 1) * 0x1B))
+#define MUL3(x) (MUL2(x) ^ (x))
+#define MUL9(x) (MUL2(MUL2(MUL2(x))) ^ (x))
+#define MUL11(x) (MUL2(MUL2(MUL2(x))) ^ MUL2(x) ^ (x))
+#define MUL13(x) (MUL2(MUL2(MUL2(x))) ^ MUL2(MUL2(x)) ^ (x))
+#define MUL14(x) (MUL2(MUL2(MUL2(x))) ^ MUL2(MUL2(x)) ^ MUL2(x))
+
+// AES round constants
+const unsigned char RCON[10] = {
+    0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1B, 0x36};
 
 // AES S-Box
-unsigned char aes_sbox[16][16] = {
+const unsigned char aes_sbox[16][16] = {
     {0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76},
     {0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0},
     {0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15},
@@ -23,26 +45,27 @@ unsigned char aes_sbox[16][16] = {
     {0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9, 0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08},
     {0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a},
     {0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e},
-    {0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf}};
+    {0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf},
+    {0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16}};
 
 // AES Inverse S-Box
-unsigned char aes_inverse_sbox[16][16] = {
-    {0x52, 0x09, 0x6A, 0xD5, 0x30, 0x36, 0xA5, 0x38, 0xBF, 0x40, 0xA3, 0x9E, 0x81, 0xF3, 0xD7, 0xFB},
-    {0x7C, 0xE3, 0x39, 0x82, 0x9B, 0x2F, 0xFF, 0x87, 0x34, 0x8E, 0x43, 0x44, 0xC4, 0xDE, 0xE9, 0xCB},
-    {0x54, 0x7B, 0x94, 0x32, 0xA6, 0xC2, 0x23, 0x3D, 0xEE, 0x4C, 0x95, 0x0B, 0x42, 0xFA, 0xC3, 0x4E},
-    {0x08, 0x2E, 0xA1, 0x66, 0x28, 0xD9, 0x24, 0xB2, 0x76, 0x5B, 0xA2, 0x49, 0x6D, 0x8B, 0xD1, 0x25},
-    {0x72, 0xF8, 0xF6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xD4, 0xA4, 0x5C, 0xCC, 0x5D, 0x65, 0xB6, 0x92},
-    {0x6C, 0x70, 0x48, 0x50, 0xFD, 0xED, 0xB9, 0xDA, 0x5E, 0x15, 0x46, 0x57, 0xA7, 0x8D, 0x9D, 0x84},
-    {0x90, 0xD8, 0xAB, 0x00, 0x8C, 0xBC, 0xD3, 0x0A, 0xF7, 0xE4, 0x58, 0x05, 0xB8, 0xB3, 0x45, 0x06},
-    {0xD0, 0x2C, 0x1E, 0x8F, 0xCA, 0x3F, 0x0F, 0x02, 0xC1, 0xAF, 0xBD, 0x03, 0x01, 0x13, 0x8A, 0x6B},
-    {0x3A, 0x91, 0x11, 0x41, 0x4F, 0x67, 0xDC, 0xEA, 0x97, 0xF2, 0xCF, 0xCE, 0xF0, 0xB4, 0xE6, 0x73},
-    {0x96, 0xAC, 0x74, 0x22, 0xE7, 0xAD, 0x35, 0x85, 0xE2, 0xF9, 0x37, 0xE8, 0x1C, 0x75, 0xDF, 0x6E},
-    {0x47, 0xF1, 0x1A, 0x71, 0x1D, 0x29, 0xC5, 0x89, 0x6F, 0xB7, 0x62, 0x0E, 0xAA, 0x18, 0xBE, 0x1B},
-    {0xFC, 0x56, 0x3E, 0x4B, 0xC6, 0xD2, 0x79, 0x20, 0x9A, 0xDB, 0xC0, 0xFE, 0x78, 0xCD, 0x5A, 0xF4},
-    {0x1F, 0xDD, 0xA8, 0x33, 0x88, 0x07, 0xC7, 0x31, 0xB1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xEC, 0x5F},
-    {0x60, 0x51, 0x7F, 0xA9, 0x19, 0xB5, 0x4A, 0x0D, 0x2D, 0xE5, 0x7A, 0x9F, 0x93, 0xC9, 0x9C, 0xEF},
-    {0xA0, 0xE0, 0x3B, 0x4D, 0xAE, 0x2A, 0xF5, 0xB0, 0xC8, 0xEB, 0xBB, 0x3C, 0x83, 0x53, 0x99, 0x61},
-    {0x17, 0x2B, 0x04, 0x7E, 0xBA, 0x77, 0xD6, 0x26, 0xE1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0C, 0x7D}};
+const unsigned char aes_inverse_sbox[16][16] = {
+    {0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb},
+    {0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb},
+    {0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d, 0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e},
+    {0x08, 0x2e, 0xa1, 0x66, 0x28, 0xd9, 0x24, 0xb2, 0x76, 0x5b, 0xa2, 0x49, 0x6d, 0x8b, 0xd1, 0x25},
+    {0x72, 0xf8, 0xf6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xd4, 0xa4, 0x5c, 0xcc, 0x5d, 0x65, 0xb6, 0x92},
+    {0x6c, 0x70, 0x48, 0x50, 0xfd, 0xed, 0xb9, 0xda, 0x5e, 0x15, 0x46, 0x57, 0xa7, 0x8d, 0x9d, 0x84},
+    {0x90, 0xd8, 0xab, 0x00, 0x8c, 0xbc, 0xd3, 0x0a, 0xf7, 0xe4, 0x58, 0x05, 0xb8, 0xb3, 0x45, 0x06},
+    {0xd0, 0x2c, 0x1e, 0x8f, 0xca, 0x3f, 0x0f, 0x02, 0xc1, 0xaf, 0xbd, 0x03, 0x01, 0x13, 0x8a, 0x6b},
+    {0x3a, 0x91, 0x11, 0x41, 0x4f, 0x67, 0xdc, 0xea, 0x97, 0xf2, 0xcf, 0xce, 0xf0, 0xb4, 0xe6, 0x73},
+    {0x96, 0xac, 0x74, 0x22, 0xe7, 0xad, 0x35, 0x85, 0xe2, 0xf9, 0x37, 0xe8, 0x1c, 0x75, 0xdf, 0x6e},
+    {0x47, 0xf1, 0x1a, 0x71, 0x1d, 0x29, 0xc5, 0x89, 0x6f, 0xb7, 0x62, 0x0e, 0xaa, 0x18, 0xbe, 0x1b},
+    {0xfc, 0x56, 0x3e, 0x4b, 0xc6, 0xd2, 0x79, 0x20, 0x9a, 0xdb, 0xc0, 0xfe, 0x78, 0xcd, 0x5a, 0xf4},
+    {0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31, 0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f},
+    {0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef},
+    {0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61},
+    {0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d}};
 
 // Function to lookup the S-Box value
 unsigned char aes_sbox_lookup(unsigned char byte)
@@ -142,42 +165,6 @@ unsigned char *inverse_shift_rows(unsigned char *state)
     return state;
 }
 
-// Function to perform multiplication by 2 in GF(2^8)
-unsigned char mul2(unsigned char val)
-{
-    return ((val << 1) ^ (((val >> 7) & 1) * 0x1B));
-}
-
-// Function to perform multiplication by 3 in GF(2^8)
-unsigned char mul3(unsigned char val)
-{
-    return (mul2(val) ^ val);
-}
-
-// Function to perform multiplication by 9 in GF(2^8)
-unsigned char mul9(unsigned char val)
-{
-    return (mul2(mul2(mul2(val))) ^ val);
-}
-
-// Function to perform multiplication by 11 in GF(2^8)
-unsigned char mul11(unsigned char val)
-{
-    return (mul2(mul2(mul2(val))) ^ mul2(val) ^ val);
-}
-
-// Function to perform multiplication by 13 in GF(2^8)
-unsigned char mul13(unsigned char val)
-{
-    return (mul2(mul2(mul2(val))) ^ mul2(mul2(val)) ^ val);
-}
-
-// Function to perform multiplication by 14 in GF(2^8)
-unsigned char mul14(unsigned char val)
-{
-    return (mul2(mul2(mul2(val))) ^ mul2(mul2(val)) ^ mul2(val));
-}
-
 // Function to perform the MixColumns operation
 unsigned char *mix_columns(unsigned char *state)
 {
@@ -185,10 +172,10 @@ unsigned char *mix_columns(unsigned char *state)
 
     for (int i = 0; i < 4; ++i)
     {
-        tmp[4 * i + 0] = (unsigned char)(mul2(state[4 * i]) ^ mul3(state[4 * i + 1]) ^ state[4 * i + 2] ^ state[4 * i + 3]);
-        tmp[4 * i + 1] = (unsigned char)(state[4 * i] ^ mul2(state[4 * i + 1]) ^ mul3(state[4 * i + 2]) ^ state[4 * i + 3]);
-        tmp[4 * i + 2] = (unsigned char)(state[4 * i] ^ state[4 * i + 1] ^ mul2(state[4 * i + 2]) ^ mul3(state[4 * i + 3]));
-        tmp[4 * i + 3] = (unsigned char)(mul3(state[4 * i]) ^ state[4 * i + 1] ^ state[4 * i + 2] ^ mul2(state[4 * i + 3]));
+        tmp[4 * i + 0] = (unsigned char)(MUL2(state[4 * i]) ^ MUL3(state[4 * i + 1]) ^ state[4 * i + 2] ^ state[4 * i + 3]);
+        tmp[4 * i + 1] = (unsigned char)(state[4 * i] ^ MUL2(state[4 * i + 1]) ^ MUL3(state[4 * i + 2]) ^ state[4 * i + 3]);
+        tmp[4 * i + 2] = (unsigned char)(state[4 * i] ^ state[4 * i + 1] ^ MUL2(state[4 * i + 2]) ^ MUL3(state[4 * i + 3]));
+        tmp[4 * i + 3] = (unsigned char)(MUL3(state[4 * i]) ^ state[4 * i + 1] ^ state[4 * i + 2] ^ MUL2(state[4 * i + 3]));
     }
 
     for (int i = 0; i < 16; ++i)
@@ -199,17 +186,16 @@ unsigned char *mix_columns(unsigned char *state)
     return state;
 }
 
-// Function to perform the inverse MixColumns operation
 unsigned char *inverse_mix_columns(unsigned char *state)
 {
     unsigned char tmp[16];
 
     for (int i = 0; i < 4; ++i)
     {
-        tmp[4 * i + 0] = (unsigned char)(mul14(state[4 * i]) ^ mul11(state[4 * i + 1]) ^ mul13(state[4 * i + 2]) ^ mul9(state[4 * i + 3]));
-        tmp[4 * i + 1] = (unsigned char)(mul9(state[4 * i]) ^ mul14(state[4 * i + 1]) ^ mul11(state[4 * i + 2]) ^ mul13(state[4 * i + 3]));
-        tmp[4 * i + 2] = (unsigned char)(mul13(state[4 * i]) ^ mul9(state[4 * i + 1]) ^ mul14(state[4 * i + 2]) ^ mul11(state[4 * i + 3]));
-        tmp[4 * i + 3] = (unsigned char)(mul11(state[4 * i]) ^ mul13(state[4 * i + 1]) ^ mul9(state[4 * i + 2]) ^ mul14(state[4 * i + 3]));
+        tmp[4 * i + 0] = (unsigned char)(MUL14(state[4 * i]) ^ MUL11(state[4 * i + 1]) ^ MUL13(state[4 * i + 2]) ^ MUL9(state[4 * i + 3]));
+        tmp[4 * i + 1] = (unsigned char)(MUL9(state[4 * i]) ^ MUL14(state[4 * i + 1]) ^ MUL11(state[4 * i + 2]) ^ MUL13(state[4 * i + 3]));
+        tmp[4 * i + 2] = (unsigned char)(MUL13(state[4 * i]) ^ MUL9(state[4 * i + 1]) ^ MUL14(state[4 * i + 2]) ^ MUL11(state[4 * i + 3]));
+        tmp[4 * i + 3] = (unsigned char)(MUL11(state[4 * i]) ^ MUL13(state[4 * i + 1]) ^ MUL9(state[4 * i + 2]) ^ MUL14(state[4 * i + 3]));
     }
 
     for (int i = 0; i < 16; ++i)
@@ -220,70 +206,142 @@ unsigned char *inverse_mix_columns(unsigned char *state)
     return state;
 }
 
+// Function to expand the key into round keys for AES encryption
+void key_expansion(const unsigned char *input_key, unsigned char *round_keys)
+{
+
+    int i = 0;
+    while (i < BLOCK_SIZE)
+    {
+        round_keys[i] = input_key[i];
+        i++;
+    }
+    while (i < BLOCK_SIZE * (N_ROUNDS + 1))
+    {
+        unsigned char temp[4] = {round_keys[i - 4], round_keys[i - 3], round_keys[i - 2], round_keys[i - 1]};
+        if (i % BLOCK_SIZE == 0)
+        {
+            // Rotate
+            unsigned char t = temp[0];
+            temp[0] = temp[1];
+            temp[1] = temp[2];
+            temp[2] = temp[3];
+            temp[3] = t;
+
+            // S-Box Substitution
+            temp[0] = aes_sbox[temp[0] >> 4][temp[0] & 0x0F];
+            temp[1] = aes_sbox[temp[1] >> 4][temp[1] & 0x0F];
+            temp[2] = aes_sbox[temp[2] >> 4][temp[2] & 0x0F];
+            temp[3] = aes_sbox[temp[3] >> 4][temp[3] & 0x0F];
+
+            // Rcon
+            temp[0] ^= RCON[(i / BLOCK_SIZE) - 1];
+        }
+        round_keys[i] = round_keys[i - BLOCK_SIZE] ^ temp[0];
+        round_keys[i + 1] = round_keys[i - BLOCK_SIZE + 1] ^ temp[1];
+        round_keys[i + 2] = round_keys[i - BLOCK_SIZE + 2] ^ temp[2];
+        round_keys[i + 3] = round_keys[i - BLOCK_SIZE + 3] ^ temp[3];
+        i += 4;
+    }
+}
+
+// Function to perform the AddRoundKey operation
+void add_round_key(unsigned char *state, const unsigned char *round_key)
+{
+    for (int i = 0; i < BLOCK_SIZE; ++i)
+    {
+        state[i] ^= round_key[i];
+    }
+}
+
+// Function to encrypt one block of data using AES
+void aes_encrypt(unsigned char *input, unsigned char *output, unsigned char *key)
+{
+    unsigned char round_keys[176]; // 11 round keys (176 bytes total)
+
+    // Generate round keys
+    key_expansion(key, round_keys);
+
+    // Initial round
+    add_round_key(input, round_keys);
+
+    // Main rounds
+    for (int round = 1; round < N_ROUNDS; ++round)
+    {
+        sub_bytes(input);
+        shift_rows(input);
+        mix_columns(input);
+        add_round_key(input, round_keys + round * BLOCK_SIZE);
+    }
+
+    // Final round
+    sub_bytes(input);
+    shift_rows(input);
+    add_round_key(input, round_keys + N_ROUNDS * BLOCK_SIZE);
+
+    // Copy encrypted data to output
+    memcpy(output, input, BLOCK_SIZE);
+}
+
+// Function to decrypt one block of data using AES
+void aes_decrypt(unsigned char *input, unsigned char *output, unsigned char *key)
+{
+    unsigned char round_keys[176]; // 11 round keys (176 bytes total)
+
+    // Generate round keys
+    key_expansion(key, round_keys);
+
+    // Initial round
+    add_round_key(input, round_keys + N_ROUNDS * BLOCK_SIZE);
+
+    // Main rounds
+    for (int round = N_ROUNDS - 1; round > 0; --round)
+    {
+        inverse_shift_rows(input);
+        inverse_sub_bytes(input);
+        add_round_key(input, round_keys + round * BLOCK_SIZE);
+        inverse_mix_columns(input);
+    }
+
+    // Final round
+    inverse_shift_rows(input);
+    inverse_sub_bytes(input);
+    add_round_key(input, round_keys);
+
+    // Copy decrypted data to output
+    memcpy(output, input, BLOCK_SIZE);
+}
+
+
+void print_block(const unsigned char *block)
+{
+    printf("\n");
+    for (int i = 0; i < BLOCK_SIZE; ++i)
+    {
+        printf("%02x ", block[i]);
+    }
+    printf("\n\n");
+}
+
 int main()
 {
-    unsigned char state[BLOCK_SIZE] = {0x32, 0x88, 0x31, 0xe0, 0x43, 0x5a, 0x31, 0x37, 0xf6, 0x30, 0x98, 0x07, 0xa8, 0x8d, 0xa2, 0x34};
+    unsigned char plaintext[BLOCK_SIZE] = {0x32, 0x88, 0x31, 0xe0, 0x43, 0x5a, 0x31, 0x37, 0xf6, 0x30, 0x98, 0x07, 0xa8, 0x8d, 0xa2, 0x34};
 
-    printf("Original State:\n");
-    for (int i = 0; i < BLOCK_SIZE; ++i)
-    {
-        printf("%02x ", state[i]);
-    }
-    printf("\n\n");
+    unsigned char key[BLOCK_SIZE] = {0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c};
 
-    sub_bytes(state);
+    unsigned char ciphertext[BLOCK_SIZE];
+    unsigned char decrypted[BLOCK_SIZE];
 
-    printf("After SubBytes:\n");
-    for (int i = 0; i < BLOCK_SIZE; ++i)
-    {
-        printf("%02x ", state[i]);
-    }
-    printf("\n\n");
+    printf("Plaintext:\n");
+    print_block(plaintext);
 
-    shift_rows(state);
+    aes_encrypt(plaintext, ciphertext, key);
+    printf("Ciphertext:\n");
+    print_block(ciphertext);
 
-    printf("After ShiftRows:\n");
-    for (int i = 0; i < BLOCK_SIZE; ++i)
-    {
-        printf("%02x ", state[i]);
-    }
-    printf("\n\n");
-
-    mix_columns(state);
-
-    printf("After MixColumns:\n");
-    for (int i = 0; i < BLOCK_SIZE; ++i)
-    {
-        printf("%02x ", state[i]);
-    }
-    printf("\n\n");
-
-    inverse_mix_columns(state);
-
-    printf("After Inverse MixColumns:\n");
-    for (int i = 0; i < BLOCK_SIZE; ++i)
-    {
-        printf("%02x ", state[i]);
-    }
-    printf("\n\n");
-
-    inverse_shift_rows(state);
-
-    printf("After Inverse ShiftRows:\n");
-    for (int i = 0; i < BLOCK_SIZE; ++i)
-    {
-        printf("%02x ", state[i]);
-    }
-    printf("\n\n");
-
-    inverse_sub_bytes(state);
-
-    printf("After Inverse SubBytes:\n");
-    for (int i = 0; i < BLOCK_SIZE; ++i)
-    {
-        printf("%02x ", state[i]);
-    }
-    printf("\n\n");
+    aes_decrypt(ciphertext, decrypted, key);
+    printf("Decrypted:\n");
+    print_block(decrypted);
 
     return 0;
 }
